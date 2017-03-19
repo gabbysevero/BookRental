334245b0380c8e42a7d49f80a662bdd4b157e30b
DatuKamot <gamoranto@gbox.adnu.edu.ph>
2017-03-19 09:18:54 -0700

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
 	has_many :books
 	
-	validates :name, presence:true, length: {minimum: 8}
+	validates :name, presence:true, format: {with: /[A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9]/}
 	validates :created_at, presence:true
 	validates :updated_at, presence:true
 end
