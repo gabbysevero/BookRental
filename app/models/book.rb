c68614c84e712c9b2000dca371fac8258d4e73ac
DatuKamot <gamoranto@gbox.adnu.edu.ph>
2017-03-19 06:26:23 -0700

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,17 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
+	
+	validates :title, presence:true
+	validates :author, presence:true
+	validates :price, presence:true
+	validates :description, presence:true
+	validates :created_at, presence:true
+	validates :updated_at, presence:true
+	validates :user_id, presence:true
+	validates :book_img_file_name, presence:true
+	validates :book_img_file_size, presence:true
+	validates :book_img__updated_at, presence:true
 
 	has_attached_file :book_img, styles: { book_index: "300x300>", book_show: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
